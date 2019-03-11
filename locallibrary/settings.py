e59c2f3837652f63538281462a5c2bad7b5672d3
Remmom <48096542+Remmom@users.noreply.github.com>
2019-03-12 00:50:44 +0200

    Update settings.py

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost','morning-savannah-85548.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','localhost','https://morning-savannah-85548.herokuapp.com/']
 
 
 # Application definition
